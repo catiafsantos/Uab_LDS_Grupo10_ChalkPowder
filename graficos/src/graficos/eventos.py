781788aa2002118097cfbfd8ac89e89c3afe7f0b
miguelelo <72255202+miguelelo@users.noreply.github.com>
2025-04-03 23:29:14 +0100

    Adiciona Eventos

---
+++ graficos/src/graficos/eventos.py
@@ -0,0 +1,26 @@
+from typing import Callable, Any
+
+
+class Event:
+    """Classe que representa um evento para subscrição.
+
+    Esta classe pode ser instanciada na forma atual ou pode ser especializada de modo a 
+    a especificar a assinatura do handler aceite de modo mais estrito (usando type-hints) 
+    e/ou para implementar outras customizações desejadas.
+    """
+    def __init__(self):
+        self.__handlers = set()
+
+    def add_handler(self, handler: Callable) -> None:
+        self.__handlers.add(handler)
+    
+    def remove_handler(self, handler: Callable) -> None:
+        """Removes handler by object identity.
+        
+        If handler is not present, ignores request.
+        """
+        self.__handlers.discard(handler)
+    
+    def invoke(self, *args: Any, **kwargs: Any) -> None:
+        for handler in self.__handlers:
+            handler(*args, **kwargs)
