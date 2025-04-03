e0696d3833228a99cb54c7dd5f5223305c958143
miguelelo <72255202+miguelelo@users.noreply.github.com>
2025-04-04 00:23:33 +0100

    feat: Esqueleto para ImportarFicheiroClickEvt emitido pela view

--- graficos/src/graficos/controller.py
+++ graficos/src/graficos/controller.py
@@ -5,7 +5,15 @@ class Controller:
     def __init__(self):
         self.view = view = View()
         self.model = Model(view)
-        
+
+        # Subscrições de eventos da View
+        self.view.importar_ficheiro_click_evt.add_handler(self.user_importa_ficheiro)
 
     def run(self):
         self.view.iniciar_interface()
+
+    def user_importa_ficheiro(self) -> None:
+        # TODO:
+        print("Controller recebeu evento (da View) de que user seleccionou importar ficheiro")
+        # Aqui vamos da instrução à View para mostrar o file selection dialog para o user selecionar um
+        # ficheiro do file system para ser importado
